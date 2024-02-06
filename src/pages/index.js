09de71c77de6d773f2e2a3f5d7af233f9fe3a1bc
Үүрцайх Норовсамбуу <you@example.com>
2024-02-05 20:34:13 -0800

    Feb6

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,36 +4,45 @@ import Trending from "@/components/Trending";
 import React from "react";
 
 export default function Home(props) {
-  const { posts,trending,posts1} = props;
-console.log(posts)
+  const { posts, trending, posts1 } = props;
+  console.log(posts);
   return (
     <div className="flex flex-col gap-[100px] items-center justify-between">
-     <div>  {posts.map((data) => (
-        <Highlight data={data} />
-      ))}</div>
-     <div className="flex flex-col gap-[30px] items-start w-[1216px]"> 
-      <div className="text-2xl text-[#181A2A] font-bold">Trending</div>
-      <div className="w-[1216px] flex gap-[20px]">{trending.map((data1) => (
-        <Trending data1={data1} />
-      ))}</div></div>
-     <div className="flex flex-col gap-[32px] items-start w-[1216px]"> 
-     <div className="text-2xl text-[#181A2A] font-bold">All Blog Post</div>
-      <div className="flex flex-wrap gap-[20px]">{posts1.map((data2) => (
-        <BlogPost data2={data2} />
-      ))}</div></div>
+      <div>
+        {" "}
+        {posts.map((data) => (
+          <Highlight data={data} />
+        ))}
+      </div>
+      <div className="flex flex-col gap-[30px] items-start w-[1216px]">
+        <div className="text-2xl text-[#181A2A] font-bold">Trending</div>
+        <div className="w-[1216px] flex gap-[20px]">
+          {trending.map((data1) => (
+            <Trending data1={data1} />
+          ))}
+        </div>
+      </div>
+      <div className="flex flex-col gap-[32px] items-start w-[1216px]">
+        <div className="text-2xl text-[#181A2A] font-bold">All Blog Post</div>
+        <div className="flex flex-wrap gap-[20px]">
+          {posts1.map((data2) => (
+            <BlogPost data2={data2} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
 export async function getStaticProps() {
-  const result = await fetch("http://localhost:4000/api/meta");
+  const result = await fetch("http://localhost:4000/api/metaBlog/meta");
   const posts = await result.json();
 
-  const trendings = await fetch("https://dev.to/api/articles?per_page=4&top=1");
+  const trendings = await fetch("http://localhost:4000/api/metaBlog/trendings");
   const trending = await trendings.json();
 
-  const posts2 = await fetch("https://dev.to/api/articles?per_page=9");
+  const posts2 = await fetch("http://localhost:4000/api/metaBlog/blog");
   const posts1 = await posts2.json();
   return {
-    props: { posts,trending,posts1 },
+    props: { posts, trending, posts1 },
   };
 }
